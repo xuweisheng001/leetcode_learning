521ff5659d08a6c53a9e489b4bb6b8750a458269
xuweisheng001 <451059921@qq.com>
2023-01-02 11:28:35 +0800

    leetcode [2351]第一个出现两次的字母

---
+++ bytedance/字符串/main/RepeatedCharacter.go
@@ -0,0 +1,16 @@
+package main
+
+func main() {
+	s := "abccbaacz"
+	println(repeatedCharacter(s))
+}
+func repeatedCharacter(s string) byte {
+	seen := map[rune]bool{}
+	for _, c := range s {
+		if seen[c] {
+			return byte(c)
+		}
+		seen[c] = true
+	}
+	return 0
+}
